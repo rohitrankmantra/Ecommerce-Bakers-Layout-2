204c218d006466aef242eac110fee96e2f8d5f03
vikas <vikasrankmantra@gmail.com>
2026-02-02 12:29:43 +0530

    changed few data to original one

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -63,7 +63,7 @@ export default function ShopPage() {
 
       {/* Main Content */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="flex gap-8">
+        <div className="flex flex-col lg:flex-row gap-8">
           {/* Filters Sidebar */}
           <ShopFilters
             selectedCategory={selectedCategory}
