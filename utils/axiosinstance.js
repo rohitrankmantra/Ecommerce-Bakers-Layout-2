60d845457dbe4c6f8e9b2c2d2188a352c9769759
rohitrankmantra <rohitrankmantra12@gmail.com>
2026-02-05 14:51:02 +0530

    Update base URL for axios instance

--- utils/axiosinstance.js
+++ utils/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://master-bakers-backend.onrender.com',
+  baseURL: 'https://master-bakers-backend.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
