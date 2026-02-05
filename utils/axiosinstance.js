445fbc02b346129fc1ecbbb4090633d2c0a5eed6
rohitrankmantra <rohitrankmantra12@gmail.com>
2026-02-05 14:48:01 +0530

    Update API base URL to production endpoint

--- utils/axiosinstance.js
+++ utils/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/api',
+  baseURL: 'https://master-bakers-backend.onrender.com',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
