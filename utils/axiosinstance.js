54c5f71d8ac50bf69ff07658ff6eff02fa0cc10b
developmentrankmantra-sudo <developmentrankmantra@gmail.com>
2026-02-26 10:36:59 +0530

    iphone error

--- utils/axiosinstance.js
+++ utils/axiosinstance.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
+// use environment variable so we can point at Render in production
 const api = axios.create({
-  baseURL: 'https://master-bakers-backend-n9iw.onrender.com/api',
-  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://master-bakers-backend-n9iw.onrender.com/api',
+  withCredentials: true, // required for cookies
   headers: { 'Content-Type': 'application/json' },
 })
-
 
 export default api
